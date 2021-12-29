7cdeebb492161544772a1932c9e118c4f603d05b
sean <midruss@yahoo.com>
2021-12-29 14:14:49 -0500

    Set up spec files  and started #uniq problem

---
+++ spec/tdd_project_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require "tdd_projects"
+
+describe Array do
+    subject(:array) {Array.new([1, 2, 1, 3, 3])}
+    
+    describe "#uniq" do
+        it "length returns correctly" do
+        end
+    end
+end
