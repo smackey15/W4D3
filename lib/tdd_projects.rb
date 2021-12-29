77cd653317eb2d22a59d64f0bf2309c4d289c6a3
sean <midruss@yahoo.com>
2021-12-29 14:51:28 -0500

    Completed specs and code for  #my_uniq and started specs for #two_sum

--- lib/tdd_projects.rb
+++ lib/tdd_projects.rb
@@ -1,7 +1,12 @@
 class Array
 
     def my_uniq
-
-
+        result = []
+        self.each do |ele|
+            if !result.include?(ele)
+                result << ele
+            end
+        end
+        result
     end
 end
--- spec/tdd_project_spec.rb
+++ spec/tdd_project_spec.rb
@@ -2,23 +2,21 @@ require "rspec"
 require "tdd_projects"
 
 describe Array do
-    # subject(:array) {Array.new([1, 2, 1, 3, 3])}
-    subject(:array) {[1, 2, 1, 3, 3]}
-    
     
     describe "#my_uniq" do
+        subject(:array_uniq) {Array.new([1, 2, 1, 3, 3])}
+       
         it "returns an array of unique elements" do
-            expect(array.my_uniq).to eq([1,2,3])
+            expect(array_uniq.my_uniq).to eq([1,2,3])
         end
     end
 
 
+    describe "#two_sum" do
+        subject(:array_two_sum) {Array.new([-1, 0, 2, -2, 1])}
 
-
-
-
-
-
+        it "returns correct result"
+    end
 
 
 end
