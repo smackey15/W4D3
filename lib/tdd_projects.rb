cc84b7838ad53fb9839a7316c98facbf1ac992b7
swusteven <swusteven@yahoo.com>
2021-12-29 14:32:42 -0500

    working on problem my_uniq

---
+++ lib/tdd_projects.rb
@@ -0,0 +1,7 @@
+class Array
+
+    def my_uniq
+
+
+    end
+end
--- spec/tdd_project_spec.rb
+++ spec/tdd_project_spec.rb
@@ -2,10 +2,23 @@ require "rspec"
 require "tdd_projects"
 
 describe Array do
-    subject(:array) {Array.new([1, 2, 1, 3, 3])}
+    # subject(:array) {Array.new([1, 2, 1, 3, 3])}
+    subject(:array) {[1, 2, 1, 3, 3]}
     
-    describe "#uniq" do
-        it "length returns correctly" do
+    
+    describe "#my_uniq" do
+        it "returns an array of unique elements" do
+            expect(array.my_uniq).to eq([1,2,3])
         end
     end
+
+
+
+
+
+
+
+
+
+
 end
