9daad6942453cf1d9e528ab81d3a3c2935e3737f
swusteven <swusteven@yahoo.com>
2021-12-29 15:21:58 -0500

    Wrote testing code for two sum that tests correct pairs of positions were returned

--- lib/tdd_projects.rb
+++ lib/tdd_projects.rb
@@ -9,4 +9,20 @@ class Array
         end
         result
     end
+
+
+    def two_sum
+        sums = []
+        
+        self.each_with_index do |num1, i1|
+            self.each_with_index do |num2, i2|
+                if i2 > i1 && num1 + num2 == 0 
+                    sums << [i1, i2]
+                end
+            end
+        end
+
+        sums
+    end
+
 end
--- spec/tdd_project_spec.rb
+++ spec/tdd_project_spec.rb
@@ -14,8 +14,14 @@ describe Array do
 
     describe "#two_sum" do
         subject(:array_two_sum) {Array.new([-1, 0, 2, -2, 1])}
+        # [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
 
-        it "returns correct result"
+        it "returns all pairs of positions where the elements at those positions sum to zero" do 
+            expect(array_two_sum.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        
+
     end
 
 
